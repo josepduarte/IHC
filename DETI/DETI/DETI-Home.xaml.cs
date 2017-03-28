3456889abcc69241c794755b2a015081d59d6e0a
José Pedro Duarte <josepduarte@ua.pt>
2017-03-28 11:37:57 +0100

    Aula2_Wpf_initial

---
+++ DETI/DETI/DETI-Home.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DETI
+{
+    /// <summary>
+    /// Interaction logic for DETI_Home.xaml
+    /// </summary>
+    public partial class DETI_Home : Page
+    {
+        public DETI_Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DETI_Cursos cursosPage = new DETI_Cursos();
+            this.NavigationService.Navigate(cursosPage);
+        }
+    }
+}
