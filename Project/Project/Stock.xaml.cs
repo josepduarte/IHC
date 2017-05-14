78777eb9dfaded37958d7d3da8d8ad5b04c4562f
Nelson Filipe dos Santos Costa <nelson.costa@ua.pt>
2017-05-14 02:50:12 +0100

    Stock page added and Homepage page updated

---
+++ Project/Project/Stock.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project
+{
+    /// <summary>
+    /// Interaction logic for Stock.xaml
+    /// </summary>
+    public partial class Stock : Page
+    {
+        public Stock()
+        {
+            InitializeComponent();
+        }
+
+        /* Botão "Voltar à Página Inicial" */
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService ns = NavigationService.GetNavigationService(this);
+            ns.Navigate(new Uri("Homepage.xaml", UriKind.Relative));
+        }
+    }
+}
