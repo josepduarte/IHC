25f911c48fbee3dcd56f0ef0cc8f0365593c530e
José Pedro Duarte <josepduarte@ua.pt>
2017-05-15 02:52:48 +0100

    Encomendas

--- Project/Project/Add_Encomenda.xaml.cs
+++ Project/Project/Add_Encomenda.xaml.cs
@@ -23,6 +23,28 @@ namespace Project
         public Add_Encomenda()
         {
             InitializeComponent();
+            initialize_components();
+        }
+        public void initialize_components()
+        {
+            // Nome
+            textbox_nome.Text = " Insira o nome do negócio aqui. ";
+            textbox_nome.Foreground = Brushes.AntiqueWhite;
+            // Contacto 
+            textbox_contacto.Text = " Insira o contacto aqui. ";
+            textbox_contacto.Foreground = Brushes.AntiqueWhite;
+            // Morada
+            textbox_morada.Text = "Insira a morada aqui. ";
+            textbox_morada.Foreground = Brushes.AntiqueWhite;
+            // Tipo de madeira
+            textbox_tipo_madeira.Text = "Insira o tipo de madeira aqui. ";
+            textbox_tipo_madeira.Foreground = Brushes.AntiqueWhite;
+            // Quantidade
+            textbox_quantidade.Text = "Insira a quantidade pretendida aqui. ";
+            textbox_quantidade.Foreground = Brushes.AntiqueWhite;
+            // Preço
+            textbox_preco.Text = "Insira o preço aqui. ";
+            textbox_preco.Foreground = Brushes.AntiqueWhite;
         }
 
         /* Botão "Voltar à Página Inicial" */
@@ -32,18 +54,60 @@ namespace Project
             this.NavigationService.Navigate(PaginaInicial);
         }
 
-        /* Botão "Ok" */
-        private void button_Click_1(object sender, RoutedEventArgs e)
+        private void TextBoxNome_GotFocus(object sender, RoutedEventArgs e)
         {
-            Encomendas PaginaEncomendas = new Encomendas();
-            this.NavigationService.Navigate(PaginaEncomendas);
+            textbox_nome.Text = "";
+            textbox_nome.Foreground = Brushes.AntiqueWhite;
         }
 
-        /* Botão "Cancelar" */
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void TextBoxContacto_GotFocus(object sender, RoutedEventArgs e)
         {
-            Encomendas PaginaEncomendas = new Encomendas();
-            this.NavigationService.Navigate(PaginaEncomendas);
+            textbox_contacto.Text = "";
+            textbox_contacto.Foreground = Brushes.AntiqueWhite;
+        }
+        private void TextBoxMorada_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textbox_morada.Text = "";
+            textbox_morada.Foreground = Brushes.AntiqueWhite;
+        }
+        private void TextBox_tipo_madeira_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textbox_tipo_madeira.Text = "";
+            textbox_tipo_madeira.Foreground = Brushes.AntiqueWhite;
+        }
+        private void TextBox_quantidade_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textbox_quantidade.Text = "";
+            textbox_quantidade.Foreground = Brushes.AntiqueWhite;
+        }
+        private void TextBox_preco_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textbox_preco.Text = "";
+            textbox_preco.Foreground = Brushes.AntiqueWhite;
+        }
+
+        private void Adicionar(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                String cliente = textbox_nome.Text;
+                int contacto = (Convert.ToInt32(textbox_contacto.Text));
+                String morada = textbox_morada.Text;
+                DateTime _data = Convert.ToDateTime(data.Text);
+                String tipo_madeira = textbox_tipo_madeira.Text;
+                String quantidade = textbox_quantidade.Text;
+                double preco = (Convert.ToDouble(textbox_preco.Text));
+
+                ListaEncomendas.getLista().add_Encomenda(cliente, contacto, morada, tipo_madeira, quantidade, preco, _data);
+
+                MessageBox.Show("Encomenda criada. ");
+                this.NavigationService.Refresh();
+                initialize_components();
+            }
+            catch
+            {
+                MessageBox.Show("Erro na introdução dos dados. ");
+            }
         }
     }
 }
--- Project/Project/Encomendas.xaml.cs
+++ Project/Project/Encomendas.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +30,11 @@ namespace Project
             InitializeComponent();
         }
 
+        public Encomendas(object data) : this()
+        {
+            this.DataContext = data;
+        }
+
         /* Botão "Voltar à Página Inicial" */
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -41,5 +48,101 @@ namespace Project
             NavigationService ns = NavigationService.GetNavigationService(this);
             ns.Navigate(new Uri("Add_Encomenda.xaml", UriKind.Relative));
         }
+
+        private void Apagar_encomenda(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.NavigationService.Refresh();
+                String cliente = Convert.ToString(TypeDescriptor.GetProperties(this.encomendasListBox.SelectedItem)["cliente"].GetValue(this.encomendasListBox.SelectedItem));
+
+                Encomenda toRemove = ListaEncomendas.getLista().Single(r => r.cliente == cliente); // && r.contacto == contacto && r.morada == morada && (r.inicio.CompareTo(inicio) == 0) && (r.fim.CompareTo(fim) == 0) && r.descricao == descricao);
+                ListaEncomendas.getLista().Remove(toRemove);
+                MessageBox.Show("Encomenda Apagada. ");
+                this.NavigationService.Refresh();
+            }
+            catch{
+                MessageBox.Show("Nenhuma encomenda selecionada. ");
+                this.NavigationService.Refresh();
+            }
+            
+        }
+
+        private void Adicionar_encomenda(object sender, RoutedEventArgs e)
+        {
+           Add_Encomenda PaginaAddEncomenda = new Add_Encomenda();
+           this.NavigationService.Navigate(PaginaAddEncomenda);
+        }
     }
+    public class Encomenda
+    {
+        private string _cliente;
+        private int _contacto;
+        private string _morada;
+        private string _tipo_madeira;
+        private string _quantidade;
+        private double _preco;
+        private DateTime _data;
+
+
+        public string cliente
+        {
+            get { return _cliente; }
+            set { _cliente = value; }
+        }
+        public int contacto
+        {
+            get { return _contacto; }
+            set { _contacto = value; }
+        }
+        public string morada
+        {
+            get { return _morada; }
+            set { _morada = value; }
+        }
+        public string tipo_madeira
+        {
+            get { return _tipo_madeira; }
+            set { _tipo_madeira = value; }
+        }
+        public string quantidade
+        {
+            get { return _quantidade; }
+            set { _quantidade = value; }
+        }
+        public double preco
+        {
+            get { return _preco; }
+            set { _preco = value; }
+        }
+        public DateTime data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+    }
+
+    public class ListaEncomendas : ObservableCollection<Encomenda>
+    {
+        static ListaEncomendas lista = new ListaEncomendas();
+        public ListaEncomendas()
+        {
+            add_Encomenda("Maria Madeira Eu Quero", 93423221, "nº102, Rua das Plantas", "Eucalipto", "20 m2", 100.57, Convert.ToDateTime("2017-05-10"));
+            add_Encomenda("Carlos Josefiino Andrade", 1234212, "nº10, Rua dos Arrozes Perfeito", "Sobreiro", "40 m2", 180.50, Convert.ToDateTime("2017-05-12"));
+            add_Encomenda("Marta Carvalho", 9123121, "nº 20, Rua dos Carvalhos Perdidos", "Carvalhos", "10 m2", 50.05, Convert.ToDateTime("2017-10-11"));
+            lista = this;
+
+        }
+        public static ListaEncomendas getLista()
+        {
+            return lista;
+        }
+        public void add_Encomenda(string cliente, int contacto, string morada, string tipo_madeira, string quantidade, double preco, DateTime data)
+        {
+            this.Add(new Encomenda { cliente = cliente, contacto = contacto, morada = morada, tipo_madeira = tipo_madeira, quantidade = quantidade, preco = preco, data = data });
+
+        }
+    }
+
 }
