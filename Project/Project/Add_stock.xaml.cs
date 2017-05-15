7f8e3711744b08ddad90739bd29527cc82f8df0a
José Pedro Duarte <josepduarte@ua.pt>
2017-05-15 19:40:03 +0100

    Stock: add and delete. Update missing

---
+++ Project/Project/Add_stock.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project
+{
+    /// <summary>
+    /// Interaction logic for Add_stock.xaml
+    /// </summary>
+    public partial class Add_stock : Page
+    {
+        public Add_stock()
+        {
+            InitializeComponent();
+            initialize_components();
+        }
+        public void initialize_components()
+        {
+            // Nome
+            textbox_tipo.Text = " Insira o tipo aqui. ";
+            textbox_tipo.Foreground = Brushes.AntiqueWhite;
+            // Quantidade
+            textbox_quantidade.Text = " Insira a quantidade aqui. ";
+            textbox_quantidade.Foreground = Brushes.AntiqueWhite;
+        }
+
+        private void Adicionar(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                String tipo = textbox_tipo.Text;
+                String quantidade = (String.Concat(textbox_quantidade.Text, " m2"));
+      
+                double temp = Convert.ToDouble(textbox_quantidade.Text);
+
+                ListaMadeiras.getLista().add_Madeira(tipo, quantidade);
+
+                MessageBox.Show("Tipo de mandeira e quantidade adicionados. ");
+                this.NavigationService.Refresh();
+                initialize_components();
+            }
+            catch
+            {
+                MessageBox.Show("Erro na introdução dos dados. ");
+            }
+        }
+
+        private void textbox_quantidade_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textbox_quantidade.Text = "";
+            textbox_quantidade.Foreground = Brushes.AntiqueWhite;
+        }
+
+        private void textbox_tipo_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textbox_tipo.Text = "";
+            textbox_tipo.Foreground = Brushes.AntiqueWhite;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService ns = NavigationService.GetNavigationService(this);
+            ns.Navigate(new Uri("Homepage.xaml", UriKind.Relative));
+        }
+    }
+}
--- Project/Project/Stock.xaml.cs
+++ Project/Project/Stock.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +32,74 @@ namespace Project
         {
             NavigationService ns = NavigationService.GetNavigationService(this);
             ns.Navigate(new Uri("Homepage.xaml", UriKind.Relative));
+        }
+
+        private void Apagar_tipo(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+                String tipo = Convert.ToString(TypeDescriptor.GetProperties(this.stockListBox.SelectedItem)["tipo"].GetValue(this.stockListBox.SelectedItem));
+
+                Madeira toRemove = ListaMadeiras.getLista().Single(r => r.tipo == tipo); // && r.contacto == contacto && r.morada == morada && (r.inicio.CompareTo(inicio) == 0) && (r.fim.CompareTo(fim) == 0) && r.descricao == descricao);
+                ListaMadeiras.getLista().Remove(toRemove);
+                MessageBox.Show("Tipo de madeira apagado. ");
+                this.NavigationService.Refresh();
+            }
+            catch
+            {
+                MessageBox.Show("Nenhuma encomenda selecionada. ");
+                this.NavigationService.Refresh();
+            }
+        }
+
+        private void Adicionar_tipo(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Add_stock());
+        }
+
+        private void Atualizar_tipo(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Update_stock());
+        }
+    }
+    public class Madeira
+    {
+        private string _tipo;
+        private string _quantidade;
+
+
+        public string tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+        public string quantidade
+        {
+            get { return _quantidade; }
+            set { _quantidade = value; }
+        }
+    }
+
+    public class ListaMadeiras : ObservableCollection<Madeira>
+    {
+        static ListaMadeiras lista = new ListaMadeiras();
+        public ListaMadeiras()
+        {
+            add_Madeira("Eucalipto", "100 m2");
+            add_Madeira("Carvalho", "240 m2");
+            add_Madeira("Sobreiro", "70 m2");
+            lista = this;
+
+        }
+        public static ListaMadeiras getLista()
+        {
+            return lista;
+        }
+        public void add_Madeira(string tipo, string quantidade)
+        {
+            this.Add(new Madeira { tipo = tipo, quantidade = quantidade });
+
         }
     }
 }
