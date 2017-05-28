fa988217c76b69a06ec263531443a9df567bf2c1
José Pedro Duarte <josepduarte@ua.pt>
2017-05-28 20:03:34 +0100

    Stock page: - sliders - reset and save values

--- Project/Project/Stock.xaml.cs
+++ Project/Project/Stock.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,12 +21,23 @@ namespace Project
     /// <summary>
     /// Interaction logic for Stock.xaml
     /// </summary>
+    /// 
+
     public partial class Stock : Page
     {
         public Stock()
         {
+
             InitializeComponent();
+            
+            to_save = new ListaMadeiras((ListaMadeiras.getLista().Select(madeira => new Madeira(madeira.tipo, madeira.quantidade)).ToList()));
+            foreach (Madeira element in to_save)
+            {
+                System.Console.WriteLine(element.quantidade);
+            }
+
         }
+        ListaMadeiras to_save;
 
         /* Botão "Voltar à Página Inicial" */
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -38,17 +50,22 @@ namespace Project
         {
             try
             {
-                
                 String tipo = Convert.ToString(TypeDescriptor.GetProperties(this.stockListBox.SelectedItem)["tipo"].GetValue(this.stockListBox.SelectedItem));
+                tipo.ToString();
+                System.Windows.Controls.Button delete = (System.Windows.Controls.Button)e.OriginalSource;
+                DialogResult dialogResult = form1.Show("Confirmar remoção", "Tem a certeza que pretende apagar?", "", "Sim", "Não");
+                if (dialogResult == DialogResult.Yes)
+                { 
 
-                Madeira toRemove = ListaMadeiras.getLista().Single(r => r.tipo == tipo); // && r.contacto == contacto && r.morada == morada && (r.inicio.CompareTo(inicio) == 0) && (r.fim.CompareTo(fim) == 0) && r.descricao == descricao);
-                ListaMadeiras.getLista().Remove(toRemove);
-                MessageBox.Show("Tipo de madeira apagado. ");
-                this.NavigationService.Refresh();
+                    Madeira toRemove = ListaMadeiras.getLista().Single(r => r.tipo == tipo); // && r.contacto == contacto && r.morada == morada && (r.inicio.CompareTo(inicio) == 0) && (r.fim.CompareTo(fim) == 0) && r.descricao == descricao);
+                    ListaMadeiras.getLista().Remove(toRemove);
+                    System.Windows.MessageBox.Show("Tipo de madeira apagado. ");
+                    this.NavigationService.Refresh();
+                }                
             }
             catch
             {
-                MessageBox.Show("Nenhuma encomenda selecionada. ");
+                System.Windows.MessageBox.Show("Nenhum tipo selecionado. ");
                 this.NavigationService.Refresh();
             }
         }
@@ -67,12 +84,61 @@ namespace Project
         {
 
         }
+
+       
+        private childItem FindVisualChild<childItem>(DependencyObject obj) where childItem : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is childItem)
+                    return (childItem)child;
+                else
+                {
+                    childItem childOfChild = FindVisualChild<childItem>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
+        }
+
+        private void restaurar_valor(object sender, RoutedEventArgs e)
+        {
+            stockListBox.ItemsSource = null;
+            foreach (Madeira element in ListaMadeiras.getLista())
+            {
+                if((to_save.ToList().Find(x => x.tipo == element.tipo)) != null){
+                    element.quantidade = to_save.ToList().Find(x => x.tipo == element.tipo).quantidade;
+                }
+            }
+            stockListBox.ItemsSource = ListaMadeiras.getLista();
+
+            this.NavigationService.Refresh();
+            
+        }
+
+        private void guardar(object sender, RoutedEventArgs e)
+        {
+            foreach (Madeira element in to_save)
+            {
+                if ((ListaMadeiras.getLista().ToList().Find(x => x.tipo == element.tipo)) != null)
+                {
+                    element.quantidade = ListaMadeiras.getLista().ToList().Find(x => x.tipo == element.tipo).quantidade;
+                }
+            }
+        }
     }
     public class Madeira
     {
         private string _tipo;
         private string _quantidade;
 
+        public Madeira(string tipo, string quantidade)
+        {
+            this.tipo = tipo;
+            this.quantidade = quantidade;
+        }
 
         public string tipo
         {
@@ -91,11 +157,14 @@ namespace Project
         static ListaMadeiras lista = new ListaMadeiras();
         public ListaMadeiras()
         {
-            add_Madeira("Eucalipto", "100 m2");
-            add_Madeira("Carvalho", "240 m2");
-            add_Madeira("Sobreiro", "70 m2");
+            add_Madeira("Eucalipto", "100");
+            add_Madeira("Carvalho", "240");
+            add_Madeira("Sobreiro", "70");
             lista = this;
 
+        }
+        public ListaMadeiras(IList<Madeira> source) : base(source)
+        {
         }
         public static ListaMadeiras getLista()
         {
@@ -103,7 +172,7 @@ namespace Project
         }
         public void add_Madeira(string tipo, string quantidade)
         {
-            this.Add(new Madeira { tipo = tipo, quantidade = quantidade });
+            this.Add(new Madeira(tipo,quantidade));
 
         }
     }
